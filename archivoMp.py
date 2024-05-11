d3ef21d3ec3c2e74d065ac446623cf1b1a6c7f25
Mario <marioporlopez@hotmail.com>
2024-05-11 12:14:29 -0500

    actuakizar

---
+++ archivoMp.py
@@ -0,0 +1,8 @@
+num = int(input("Número a multiplicar  "))
+ 
+rango = range(1,11)
+for element in rango:
+	product = num * element
+	print (num, '*',	element, "=", product)
+
+print("Mario Portillo")
